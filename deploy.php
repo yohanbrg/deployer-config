6093a7f02333cbb21cabc8359fc2d47a2c3870cc
Yohan Bourgogne <ybourgogne@tequilarapido.com>
2019-04-05 15:08:25 +0200

    new conf

--- deploy.php
+++ deploy.php
@@ -4,21 +4,45 @@ namespace Deployer;
 require 'recipe/common.php';
 
 // Project name
-set('application', 'deployer');
+set('application', 'my_project');
 
-// Repository
+// Project repository
 set('repository', 'git@github.com:yohanbrg/deployer-test.git');
 
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server 
+set('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
 // Hosts
+
 host('31.15.27.87')
     ->user('cbuilder_ssh')
-    ->set('deploy_path', '/var/www/tki72/sites/cbuilder');    
+    ->set('deploy_path', '~/{{application}}');    
     
 // Tasks
+
 desc('Deploy your project');
 task('deploy', [
+    'deploy:info',
     'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
     'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
 ]);
 
 // [Optional] If deploy fails automatically unlock.
---
+++ deploy_old.php
@@ -0,0 +1,25 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'deployer');
+
+// Repository
+set('repository', 'git@github.com:yohanbrg/deployer-test.git');
+
+// Hosts
+host('31.15.27.87')
+    ->user('cbuilder_ssh')
+    ->set('deploy_path', '/var/www/tki72/sites/cbuilder');    
+    
+// Tasks
+desc('Deploy your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:update_code',
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
